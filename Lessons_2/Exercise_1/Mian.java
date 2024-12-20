a9de41c675608d8b0c2a2dc49217c1ff0f5577e7
RTK <126328596+rtk666a@users.noreply.github.com>
2024-12-20 06:07:21 +0600

    java

--- Lessons_2/Exercise_1/Mian.java
+++ Lessons_2/Exercise_1/Mian.java
@@ -1,18 +1,16 @@
+package Exercise_1;
+
 import java.util.Scanner;
 
-
-public class Main {
+public class Mian {
     public static void main(String[] args) {
         System.out.println("Type a number between 50 and 300");
         Scanner scanner = new Scanner(System.in);
-
         int a = scanner.nextInt();
-
-        if (a>=50 && a<=300){
+        if (a>=50&&a<=300) {
             System.out.println("yes!");
         }else {
             System.out.println("no!");
         }
-
     }
 }
